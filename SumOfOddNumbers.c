fabcbe23ed320ba76a456d90ddd281d8052895f5
Din Islam Sheikh <sheikhdin979@gmail.com>
2025-12-03 23:42:27 +0530

    Add program to calculate sum of odd numbers

---
+++ SumOfOddNumbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, sum = 0;
+
+    printf("Enter the limit: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i += 2) {
+        sum += i;
+    }
+
+    printf("Sum of odd numbers = %d\n", sum);
+
+    return 0;
+}
